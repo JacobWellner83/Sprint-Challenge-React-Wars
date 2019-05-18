27c3fe09b3586327666511239706244c90792de6
JacobWellner83 <jcwellner83@gmail.com>
2019-05-18 11:21:33 -0500

    Built out CharacterCard to pull in Name,b.y.,gender, height and mass.

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -1,5 +1,17 @@
 import React from 'react'
 import './StarWars.css'
 
+const CharacterCard = (props) => {
+    const { character } = props
+    return (
+        <div className="character-file">
+            <p className="name">{character.name}</p>
+            <p><span className="param">birth year</span> {character.birth_year}</p>
+            <p><span className="param">gender</span> {character.gender}</p>
+            <p><span className="param">height</span> {character.height}</p>
+            <p><span className="param">mass</span> {character.mass}</p>
+        </div>
+    )
+}
 
 export default CharacterCard;
--- starwars/src/components/CharacterList.js
+++ starwars/src/components/CharacterList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './StarWars.css'
 
+import CharacterCard from './CharacterCard'
 
 export default CharacterList;
