de910f5c6524f416136a8a0e2060b95898154755
JacobWellner83 <jcwellner83@gmail.com>
2019-05-18 11:25:38 -0500

    CharacterList linked with references

--- starwars/src/components/CharacterList.js
+++ starwars/src/components/CharacterList.js
@@ -3,4 +3,17 @@ import './StarWars.css'
 
 import CharacterCard from './CharacterCard'
 
+const CharacterList = (props) => {
+    const {characters } = props
+    return (
+        <div className="character-container">
+            {
+                characters.map(character => {
+                    return <CharacterCard key={character.name} character={character} />
+                })
+            }
+        </div>
+    )
+}
+
 export default CharacterList;
